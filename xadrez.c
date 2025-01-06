2e06aa7c1c8c427708ea866aa9b9d13b4c40df13
SuadAtse <matheusdacostaalves@gmail.com>
2025-01-06 14:18:34 +0000

    Criei á tarefa de xadres do nivel novato

--- xadrez.c
+++ xadrez.c
@@ -17,6 +17,50 @@ int main() {
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
+    const int casabispo = 5, casatorre = 5, casarainha = 8;
+    int move, set;
+    int i = 1;
+
+    printf("Escolha qual jogada você irá realizar?\n");
+    printf("Mover o Bispo em diagonal - aperte 1\n");
+    printf("Mover a Torre para à direita - aperte 2\n");
+    printf("Mover a Rainha para à esquerda - aperte 3\n");
+    printf("Escolha: ");
+    scanf("%d", &move);
+
+    switch (move)
+    {
+    case 1:
+        printf("O Bispo pode andar até 5 casas, quantas casas você vai movelo: ");
+        scanf("%d", &set);
+        while (set <= casabispo && i <= set)
+        {
+            printf("O Bispo se moveu %i vezes para à diagonal.\n",i);
+            i++;
+        }
+        break;
+    case 2:
+        printf("A Torre pode andar até 5 casas, quantas casas você vai movela: ");
+        scanf("%d", &set);
+        while (set <= casatorre && i <= set)
+        {
+            printf("A torre se moveu %i vezes para à direita.\n",i);
+            i++;
+        }
+        break;
+    case 3:
+        printf("A Rainha pode andar até 8 casas, quantas casas você vai movela: ");
+        scanf("%d", &set);
+        while (set <= casarainha && i <= set)
+        {
+            printf("A rainha se moveu %d vezes para à esquerda.\n",i);
+            i++;
+        }
+        break;
+    default:
+        printf("ERRO.Tente Novamente.");
+    }
+
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
